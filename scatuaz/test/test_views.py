68c18e3563e8210ab2b238b7f783c42f43136e07
Anahí Berumen Murillo <anahi.berumen.m@gmail.com>
2020-02-12 08:20:45 -0600

    a

--- scatuaz/test/test_views.py
+++ scatuaz/test/test_views.py
@@ -7,7 +7,7 @@ class Test_View_eliminar(TestCase):
     def test_url_trabajadores_eliminar(self):
         response = self.client.get('/trabajador/eliminar')
         self.assertEqual(response.status_code, 200)    
- 
+
     def test_template_trabajador_eliminar(self):
         response = self.client.get('/trabajador/eliminar')
         self.assertTemplateUsed(response, 'confirm_delete.html')        
